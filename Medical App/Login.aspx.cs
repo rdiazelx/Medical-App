ba09d337a99aeb5e885803d3e4ea06ac28f18571
rdiazelx <rdiazelxcog0342@hotmail.com>
2023-06-11 20:55:59 -0600

    cambios de prueba

--- Medical App/Login.aspx.cs
+++ Medical App/Login.aspx.cs
@@ -11,9 +11,7 @@ namespace Medical_App
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //prueba1
-            //prueba2
-
+           
 
         }
     }
